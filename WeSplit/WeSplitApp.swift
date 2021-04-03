da2979bb4990cf01e17c209eba40f0f82d55cb97
Andrew Garcia <ucondrew@icloud.com>
2021-04-03 13:30:28 -0700

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Andrew Garcia on 4/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
