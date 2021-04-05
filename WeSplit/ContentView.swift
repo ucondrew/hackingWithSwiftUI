bf07ac0aca58a545c5c6adb810c036d288bc799c
Andrew Garcia <ucondrew@icloud.com>
2021-04-05 00:20:52 -0700

    Project 1 of Hacking With SwiftUI

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -8,11 +8,63 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var checkAmount = ""  //@State because values change during one instance of view
+    @State private var numberOfPeople = "2"
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(Int(numberOfPeople) ?? 2 + 2)
+        let tipSelection = Double(tipPercentages[tipPercentage])
+        let orderAmount = Double(checkAmount) ?? 0
+        
+        let tipValue = orderAmount / 100 * tipSelection
+        let grandTotal = orderAmount + tipValue
+        let amountPerPerson = grandTotal / peopleCount
+        
+        return amountPerPerson
+    }
+    var grandTotal: Double {
+        let people = Double(Int(numberOfPeople) ?? 2 + 2)
+        let total = totalPerPerson * people
+        return total
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView { //This creates the navigation bar
+            Form { //creates form in the view
+                Section { //adds a section including a textfield for user input and a picker
+                    TextField("Check Amount", text: $checkAmount) //$ two way binds variable to be updated
+                        .keyboardType(.decimalPad) //keypad instead of keyboard
+                    TextField("Number of People", text: $numberOfPeople) //another textfield for # of ppl
+                        .keyboardType(.numberPad) //numberpad for integers only
+                }
+                Section(header: Text("Tip Percentage")) { //second section which contains a title
+                    
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(0 ..< tipPercentages.count) { //loops through however many percentages there are
+                            Text("\(self.tipPercentages[$0])%") //displays item in array
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle()) //changed from default picker to horizontal tab picker
+                }
+                
+                Section(header: Text("Amount per person")) {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
+                
+                Section(header: Text("Total check amount")) {
+                    Text("$\(grandTotal, specifier: "%.2f")")
+                }
+                
+                
+            }
+            .navigationBarTitle("Check Total With Tip") //title of the form
+        }
     }
 }
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
