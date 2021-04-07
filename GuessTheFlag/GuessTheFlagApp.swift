3262e11cc7df7ccaf90ecefc1bd18589a6a12386
Andrew Garcia <ucondrew@icloud.com>
2021-04-07 01:45:35 -0700

    First iteration of country flag viewer app

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Andrew Garcia on 4/6/21.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
